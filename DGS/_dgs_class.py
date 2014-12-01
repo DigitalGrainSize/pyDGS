c80a1dec516f985cf55d930941415dac1e042902
Daniel Buscombe <dbuscombe@usgs.gov>
2014-12-01 07:36:32 -0700

    Mon Dec  1 07:36:32 MST 2014

--- DGS/_dgs_class.py
+++ DGS/_dgs_class.py
@@ -112,6 +112,97 @@ def writeout( outfolder, item, sz, pdf ):
     print 'psd results saved to '+outfolder+os.sep+fileBaseName+'_psd.txt'
 
 # =========================================================
+def sgolay2d_fallback( z, window_size, order, derivative=None):
+    """
+    do 2d filtering on matrix
+    from http://www.scipy.org/Cookbook/SavitzkyGolay
+    """
+    # number of terms in the polynomial expression
+    n_terms = ( order + 1 ) * ( order + 2)  / 2.0
+
+    if  window_size % 2 == 0:
+        raise ValueError('window_size must be odd')
+
+    if window_size**2 < n_terms:
+        raise ValueError('order is too high for the window size')
+
+    half_size = window_size // 2
+
+    # exponents of the polynomial. 
+    # p(x,y) = a0 + a1*x + a2*y + a3*x^2 + a4*y^2 + a5*x*y + ... 
+    # this line gives a list of two item tuple. Each tuple contains 
+    # the exponents of the k-th term. First element of tuple is for x
+    # second element for y.
+    # Ex. exps = [(0,0), (1,0), (0,1), (2,0), (1,1), (0,2), ...]
+    exps = [ (k-n, n) for k in range(order+1) for n in range(k+1) ]
+
+    # coordinates of points
+    ind = np.arange(-half_size, half_size+1, dtype=np.float64)
+    dx = np.repeat( ind, window_size )
+    dy = np.tile( ind, [window_size, 1]).reshape(window_size**2, )
+
+    # build matrix of system of equation
+    A = np.empty( (window_size**2, len(exps)) )
+    for i, exp in enumerate( exps ):
+        A[:,i] = (dx**exp[0]) * (dy**exp[1])
+
+    # pad input array with appropriate values at the four borders
+    new_shape = z.shape[0] + 2*half_size, z.shape[1] + 2*half_size
+    Z = np.zeros( (new_shape) )
+    # top band
+    band = z[0, :]
+    Z[:half_size, half_size:-half_size] =  band -  np.abs( np.flipud( z[1:half_size+1, :] ) - band )
+    # bottom band
+    band = z[-1, :]
+    Z[-half_size:, half_size:-half_size] = band  + np.abs( np.flipud( z[-half_size-1:-1, :] )  -band )
+    # left band
+    band = np.tile( z[:,0].reshape(-1,1), [1,half_size])
+    Z[half_size:-half_size, :half_size] = band - np.abs( np.fliplr( z[:, 1:half_size+1] ) - band )
+    # right band
+    band = np.tile( z[:,-1].reshape(-1,1), [1,half_size] )
+    Z[half_size:-half_size, -half_size:] =  band + np.abs( np.fliplr( z[:, -half_size-1:-1] ) - band )
+    # central band
+    Z[half_size:-half_size, half_size:-half_size] = z
+
+    # top left corner
+    band = z[0,0]
+    Z[:half_size,:half_size] = band - np.abs( np.flipud(np.fliplr(z[1:half_size+1,1:half_size+1]) ) - band )
+    # bottom right corner
+    band = z[-1,-1]
+    Z[-half_size:,-half_size:] = band + np.abs( np.flipud(np.fliplr(z[-half_size-1:-1,-half_size-1:-1]) ) - band )
+
+    # top right corner
+    band = Z[half_size,-half_size:]
+    Z[:half_size,-half_size:] = band - np.abs( np.flipud(Z[half_size+1:2*half_size+1,-half_size:]) - band )
+    # bottom left corner
+    band = Z[-half_size:,half_size].reshape(-1,1)
+    Z[-half_size:,:half_size] = band - np.abs( np.fliplr(Z[-half_size:, half_size+1:2*half_size+1]) - band )
+
+    # solve system and convolve
+    if derivative == None:
+        m = np.linalg.pinv(A)[0].reshape((window_size, -1))
+        Z = Z.astype('f')
+        m = m.astype('f')
+        return sp.fftconvolve(Z, m, mode='valid')
+    elif derivative == 'col':
+        A = A.astype('f')
+        c = np.linalg.pinv(A)[1].reshape((window_size, -1))
+        Z = Z.astype('f')
+        return sp.fftconvolve(Z, -c, mode='valid')
+    elif derivative == 'row':
+        A = A.astype('f')
+        Z = Z.astype('f')
+        r = np.linalg.pinv(A)[2].reshape((window_size, -1))
+        return sp.fftconvolve(Z, -r, mode='valid')
+    elif derivative == 'both':
+        A = A.astype('f')
+        Z = Z.astype('f')
+        c = np.linalg.pinv(A)[1].reshape((window_size, -1))
+        r = np.linalg.pinv(A)[2].reshape((window_size, -1))
+        return sp.fftconvolve(Z, -r, mode='valid'), sp.fftconvolve(Z, -c, mode='valid')
+
+
+# =========================================================
 # =========================================================
 def dgs(folder, density, doplot, resolution):
 
@@ -183,7 +274,7 @@ def dgs(folder, density, doplot, resolution):
    # cover all major file types
    files1 = glob.glob(folder+os.sep+"*.JPG")
    files2 = glob.glob(folder+os.sep+"*.jpg")
-   files3 = glob.glob(folder+os.sep+"*.bmp")
+   files3 = glob.glob(folder+os.sep+"*.jpeg")
    files4 = glob.glob(folder+os.sep+"*.TIF")
    files5 = glob.glob(folder+os.sep+"*.tif")
    files6 = glob.glob(folder+os.sep+"*.TIFF")
@@ -192,6 +283,9 @@ def dgs(folder, density, doplot, resolution):
    files9 = glob.glob(folder+os.sep+"*.png")
 
    files = files1+files2+files3+files4+files5+files6+files7+files8+files9
+
+   seen = set()
+   files = [x for x in files if x not in seen and not seen.add(x)]
 
    csvfilename = outfolder+os.sep+'dgs_results.csv'
    f_csv = open(csvfilename, 'ab')
@@ -224,7 +318,9 @@ def dgs(folder, density, doplot, resolution):
       if os.name=='posix':
          Zf = sgolay.sgolay2d( region, window_size, order=3).getdata()
       else:
-         Zf = np.mean(region)
+         #Zf = np.mean(region)
+         import scipy.signal as sp # for polynomial fitting
+         Zf = sgolay2d_fallback( region, window_size, order=3)
 
       # rescale filtered image to full 8-bit range
       useregion = rescale(region-Zf,0,255)
